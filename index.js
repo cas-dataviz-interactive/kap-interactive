f33131be8f6ac2ce6716d6ea9ec2908792412040
chrischne <chrischne@gmail.com>
2019-08-05 20:24:51 +0200

    initial data load

---
+++ index.js
@@ -0,0 +1,45 @@
+var w = 400;
+var h = 250;
+
+var margin = {
+  top: 20,
+  bottom: 20,
+  left: 20,
+  right: 20
+}
+
+var width = w - margin.left - margin.right
+var height = h - margin.top - margin.bottom
+
+var data = [23, 67, 34, 56, 89, 100, 234, 134, 97];
+
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])  
+    .range([0,width]);
+
+var y = d3.scaleLinear()
+    .domain([0, data.length])  
+    .range([0,height]);
+
+var svg = d3.select("body").append("svg")
+  .attr("id", "svg")
+  .attr("width", w)
+  .attr("height", h);   
+
+var chart = svg.append('g')
+chart.selectAll(".bar")
+  .data(data)
+  .enter()
+  .append("rect")
+  .classed('bar', true)
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .attr("x", 0)
+  .attr("y", function (d, i) {
+    return y(i);
+  })
+  .attr("width", function (d, i) {
+    return x(d);
+  })
+  .attr("height", function(d,i){
+    return y(1)-1
+  });
