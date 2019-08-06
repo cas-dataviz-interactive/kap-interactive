cb807d424d917ea9de697c2cce19dd81aa841d7d
chrischne <chrischne@gmail.com>
2019-08-06 13:42:55 +0200

    initial rendering

--- index.js
+++ index.js
@@ -1,55 +1,106 @@
-var w = 600;
-var h = 800;
+var w = 1200;
+var h = 1000;
 
 var margin = {
   top: 20,
   bottom: 20,
-  left: 20,
+  left: 100,
   right: 20
 }
 
-var width = w - margin.left - margin.right
-var height = h - margin.top - margin.bottom
+var width = w - margin.left - margin.right;
+var height = h - margin.top - margin.bottom;
 
-var data = [23, 67, 34, 56, 89, 100, 234, 134, 97];
+var parseDate = d3.timeParse("%Y-%m-%d");
+var startDate = parseDate("2019-04-01");
+var endDate = parseDate("2019-08-01");
 
-d3.dsv(';', 'kap.csv', function (d) {
-  return d;
+var data = [];
+var dates = [];
+var stations = [];
+
+var x = d3.scaleTime();
+var y = d3.scalePoint();
+var r = d3.scaleLinear();
+
+d3.dsv(',', 'kap.csv', function (d) {
+  return {
+    date: parseDate(d.Datum),
+    name: d['Name Haltestelle'],
+    kap: +d['Kundengewichtete Ankunftspünktlichkeit'],
+    kapFern: +d['Kundengewichtete Ankunftspünktlichkeit Fernverkehr'],
+    kapReg: +d['Kundengewichtete Ankunftspünktlichkeit Regionalverkehr'],
+    kepAn: +d['Kundengewichtete Anschlüsse'],
+    key: d['key']
+  }
 }).then(function (csv) {
-  console.log(csv);
+  
+
+  data = csv.filter(function(d){
+    return d.date > startDate && d.date < endDate;
+  });
+
+  console.log(data);
+
+  dates = d3.set(data, function (d) {
+    return d.date;
+  }).values();
+
+  console.log(dates);
+
+  stations = d3.set(data, function (d) {
+    return d.name;
+  }).values();
+
+  console.log(stations);
+
+  var dateExtent = d3.extent(data,function(d){
+    return d.date;
+  });
+
+  console.log('dateExtent',dateExtent);
+  x.domain(dateExtent)
+    .range([0, width]);
+
+  y.domain(stations)
+    .range([0, height]);
+
+  r.domain([50,100])
+    .range([0,5]);
+
+  var svg = d3.select("#chart").append("svg")
+    .attr("id", "svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  var chart = svg.append('g').attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  chart.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", function(d){
+      return x(d.date);
+    })
+    .attr("cy", function (d, i) {
+      return y(d.name);
+    })
+    .attr("r", function (d, i) {
+      return r(d.kap);
+    })
+    .style('fill',function(d){
+      // if(d.kap == 100){
+      //   return 'green';
+      // }
+      // else if(d.kap < 80){
+      //   return 'red';
+      // }
+      // else return 'none';
+      return 'black';
+    })
+    .style('stroke','black');
+
 });
 
-var w = 600;
-var h = 800;
 
 
-var x = d3.scaleLinear()
-  .domain([0, d3.max(data)])
-  .range([0, width]);
 
-var y = d3.scaleLinear()
-  .domain([0, data.length])
-  .range([0, height]);
-
-var svg = d3.select("body").append("svg")
-  .attr("id", "svg")
-  .attr("width", w)
-  .attr("height", h);
-
-var chart = svg.append('g')
-chart.selectAll(".bar")
-  .data(data)
-  .enter()
-  .append("rect")
-  .classed('bar', true)
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  .attr("x", 0)
-  .attr("y", function (d, i) {
-    return y(i);
-  })
-  .attr("width", function (d, i) {
-    return x(d);
-  })
-  .attr("height", function (d, i) {
-    return y(1) - 1
-  });
